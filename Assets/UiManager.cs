6f25bcf432b46de2bae500dbb8f8d15c1887ef1e
Mitja <mitja_rizvic@hotmail.com>
2015-06-09 18:20:16 +0200

    Save

---
+++ Assets/UiManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour {
+
+	private string status;
+	private bool enabled;
+
+	private AudioSource[] audioSources;
+
+	private Image InfoImg;
+	private GameObject panel;
+
+	private Image tabla;
+	private Text textZirafa;
+
+	// Use this for initialization
+	void Start () {
+		status = null;
+		enabled = false;
+
+		tabla = this.transform.Find("Tabla").gameObject.GetComponent<Image> ();
+		InfoImg = this.transform.Find("InfoImg").gameObject.GetComponent<Image> ();
+		audioSources= GameObject.Find("FPSController").gameObject.GetComponents<AudioSource>();
+		panel= this.transform.Find ("PanelBlur").gameObject;
+		textZirafa= this.transform.Find("TextZirafa").gameObject.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (status != null && Input.GetKeyDown ("i")) enabled = !enabled;
+		panel.SetActive (enabled);
+		tabla.enabled= enabled;
+
+		if (enabled) {
+			if (status == "ZIRAFA") {
+				textZirafa.enabled= true;
+			}
+		}else{
+			//Izklopi vse informacije
+			textZirafa.enabled= false;
+		}
+	}
+
+	public void showInfo( string txt){
+		status = txt;
+		InfoImg.enabled = true;
+		audioSources [1].Play ();
+	}
+
+	public void hideInfo( string txt){
+		status = null;
+		enabled = false;
+		InfoImg.enabled = false;
+	}
+}
---
+++ Assets/trigerActionZirafa.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class trigerActionZirafa : MonoBehaviour {
+
+	public GameObject uiCanvas;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other){
+		Debug.Log ("enter");
+		UiManager uiManger = uiCanvas.GetComponent<UiManager>();
+		uiManger.showInfo ("ZIRAFA");
+	}
+
+	void OnTriggerExit (Collider other){
+		UiManager uiManger = uiCanvas.GetComponent<UiManager>();
+		uiManger.hideInfo ("ZIRAFA");
+	}
+}
