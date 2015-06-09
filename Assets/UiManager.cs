7e329fe9ccb86c8e8479f0e4d75c810f85a2c7b1
Mitja <mitja_rizvic@hotmail.com>
2015-06-09 18:30:53 +0200

    Save

--- Assets/UiManager.cs
+++ Assets/UiManager.cs
@@ -11,9 +11,15 @@ public class UiManager : MonoBehaviour {
 
 	private Image InfoImg;
 	private GameObject panel;
-
 	private Image tabla;
+
+	private Text textPonizani;
+	private Text textLabod;
+	private Text textSvet;
+	private Text textRibaraca;
 	private Text textZirafa;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -24,6 +30,11 @@ public class UiManager : MonoBehaviour {
 		InfoImg = this.transform.Find("InfoImg").gameObject.GetComponent<Image> ();
 		audioSources= GameObject.Find("FPSController").gameObject.GetComponents<AudioSource>();
 		panel= this.transform.Find ("PanelBlur").gameObject;
+
+		textPonizani= this.transform.Find("TextPonizani").gameObject.GetComponent<Text> ();
+		textLabod= this.transform.Find("TextLabod").gameObject.GetComponent<Text> ();
+		textSvet= this.transform.Find("TextSvet").gameObject.GetComponent<Text> ();
+		textRibaraca= this.transform.Find("TextRibaraca").gameObject.GetComponent<Text> ();
 		textZirafa= this.transform.Find("TextZirafa").gameObject.GetComponent<Text> ();
 	}
 	
@@ -34,12 +45,19 @@ public class UiManager : MonoBehaviour {
 		tabla.enabled= enabled;
 
 		if (enabled) {
-			if (status == "ZIRAFA") {
-				textZirafa.enabled= true;
-			}
+			if (status == "PONIZANI") textPonizani.enabled= true;
+			else if( status == "LABOD" ) textLabod.enabled= true;
+			else if( status == "SVET") textSvet.enabled= true;
+			else if( status == "RIBARACA" ) textRibaraca.enabled= true;
+			else if( status == "ZIRAFA" ) textZirafa.enabled= true;
 		}else{
 			//Izklopi vse informacije
+			textPonizani.enabled= false;
+			textLabod.enabled= false;
+			textSvet.enabled= false;
+			textRibaraca.enabled= false;
 			textZirafa.enabled= false;
+
 		}
 	}
 
